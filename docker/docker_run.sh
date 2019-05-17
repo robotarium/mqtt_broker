29f38a91fe13c304a45f024fb39561f5b2d88c39
pglotfel <pglotfel@gmail.com>
2019-05-17 15:35:51 -0400

    Modfying build script

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#if [ $1 == "" ]
-#then
-	#echo "First argument should be IP of host machine for the MQTT broker"
-#fi
+if [ "$1" == "" ]; then
+	echo "First argument should be IP of host machine for the MQTT broker"
+	exit
+fi
 
-docker run -d \
-	--restart=always \
-	--name=robotarium_mqtt_broker \
-	-p $1:1884:1883 \
-	-p $1:8083:8083 \
-	-p $1:8883:8883 \
-	-p $1:8084:8084 \
-	-p $1:18083:18083 \
-	emqx/emqx:latest
+sudo docker run -d \
+		--restart=always \
+		--name=robotarium_mqtt_broker \
+		-p $1:1884:1883 \
+		-p $1:8083:8083 \
+		-p $1:8883:8883 \
+		-p $1:8084:8084 \
+		-p $1:18083:18083 \
+		emqx/emqx:latest
